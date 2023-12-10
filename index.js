27a58db4625336dc8d1616e0a191ec6a68c8b4df
aadi-netizen <kumaraditya325@gmail.com>
2023-12-10 18:02:34 +0530

    Final update: nessary update in index.js and index.ejs to get the expected resuts

--- index.js
+++ index.js
@@ -25,16 +25,19 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
-
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
+  try {
+    console.log(req.body);
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`);
+    console.log(typeof(response.data));
+    const result = response.data[Math.floor(Math.random() * response.data.length)];
+    console.log(typeof(result));
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
